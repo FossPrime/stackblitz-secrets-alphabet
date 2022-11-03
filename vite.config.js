0e8cca92bcce012e56fe91c7118663d467fe7ef3
FossPrime <ray@vblip.com>
2022-11-03 17:10:37 -0500

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { readFile } from 'node:fs/promises'
+
+// Encode with `node vite.config.js "my secret message"`
+process.env.VITE_ALPHA = await readFile('/tmp/s', { encoding: 'utf-8' })
+if (import.meta?.url?.endsWith(process.argv[1]) && process.argv[2]) {
+  const orig = process.argv[2]
+  const result = orig
+    .split('')
+    .map((c) => c.replace(' ', '%20'))
+    .join(',')
+  console.log(result)
+}
+
+export default {
+  // config options
+}
