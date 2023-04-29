8e85b99484de71844dc104904b3e87cea730bc0d
Ray Foss <ray@vblip.com>
2023-04-29 09:37:52 -0500

    fix: save to tmp

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises'
 // Encode with `node vite.config.js "my secret message"`
 const E = [' :/?=&!#%']
 try {
-  process.env.VITE_ALPHA = await readFile('/s', { encoding: 'utf-8' })
+  process.env.VITE_ALPHA = await readFile('/tmp/s', { encoding: 'utf-8' })
 } catch {}
 if (import.meta?.url?.endsWith(process.argv[1]) && process.argv[2]) {
   const orig = process.argv[2]
